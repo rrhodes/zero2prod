e1c774af091e9143d621b41cc067fcbd8b5dfc49
Ross <ross.rhodes@krakenflex.com>
2024-02-12 18:43:37 +0000

    refactor: break up logic into separate files

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,3 @@
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/{name}", web::get().to(greet))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
