d0594657099f9f0eec00dda88531f8f9e7227033
Ross <ross.rhodes@krakenflex.com>
2024-03-03 14:22:39 +0000

    feat: insert subscribers into database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to PostGres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
